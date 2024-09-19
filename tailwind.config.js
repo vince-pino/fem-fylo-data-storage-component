fec35b8ea7c3eadbbf529aab8a3754c2ef9107a7
Vince Cedie Pino <vincecediepino@gmail.com>
2024-09-19 15:13:23 +0700

    add required font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "raleway": ["Raleway", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
